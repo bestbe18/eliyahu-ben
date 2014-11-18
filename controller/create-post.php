ee3ae780d9ca82093acdf7ad046436cfbaa35161
Ben Eliyahu <bestbe18@yahoo.com>
2014-11-18 11:09:19 -0800

    Completed the videos up to 30. It's done.

--- controller/create-post.php
+++ controller/create-post.php
@@ -4,8 +4,18 @@
     $connection = new mysqli($host, $username, $password, $database);
     
     $title = filter_input(INPUT_POST, "title", FILTER_SANITIZE_STRING);
-    $title = filter_input(INPUT_POST, "post", FILTER_SANITIZE_STRING);
+    $post = filter_input(INPUT_POST, "post", FILTER_SANITIZE_STRING);
     
-    $query = $connection->query("INSERT INTO posts");     
+    $query = $connection->query("INSERT INTO posts SET title = $title', post = '$post' ");     
+   
+    if($query) {
+         echo "<p>Successfully inserted post: $title</p>";
+    }
+    else {
+        echo "<p>$connection->error</p>";
+    }
     
-    $connection->close();
+         
+         
+         
+         $connection->close();
