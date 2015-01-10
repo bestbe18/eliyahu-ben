712aa838572219dbd2878b21cf249d065742a7f7
Ben Eliyahu <bestbe18@yahoo.com>
2015-01-10 15:14:41 -0800

    no message

--- ArgumentsandTypes.php
+++ ArgumentsandTypes.php
@@ -8,4 +8,7 @@ var_dump(is_string("abc"));
 var_dump(is_string("ABC"));
 var_dump(is_string(568));
 var_dump(is_string(true));
+var_dump(is_integer(45));
+var_dump(is_integer(46.6));
+var_dump(is_bool(true));
 ?>
